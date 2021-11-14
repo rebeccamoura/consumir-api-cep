c21e52eec41c04aae0a931b7f77553f68d16e1c8
Rebecca Moura <rebeccaaruom@gmail.com>
2021-11-14 13:09:28 -0300

    Transformando a funcao para async

--- script.js
+++ script.js
@@ -2,25 +2,22 @@ const q = element => document.querySelector(element)
 const qAll = elements => document.querySelectorAll(elements)
 
 
-function getAddress(event) {
+async function getAddress(event) {
     event.preventDefault()
-    
-    let cep = q('input[name="cep"]').value
-    fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then( (result) => result.json() )
-    .then( (result) => {
-        q('input[name="cep"]').value = result.cep
-        q('input[name="address"]').value = result.logradouro
-        q('input[name="address"]').setAttribute('disabled', 'disabled')
-        q('input[name="neighborhood"]').value = result.bairro
-        q('input[name="neighborhood"]').setAttribute('disabled', 'disabled')
-        q('input[name="city"]').value = result.localidade
-        q('input[name="state"]').value = result.uf
-        q('input[name="state"]').setAttribute('disabled', 'disabled')
 
-        console.log(result)
-    })
-    .catch( () => alert('Revise os campos e tente novamente.') )
+    let cep = q('input[name="cep"]').value
+    let req = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    let json = await req.json()
+
+    q('input[name="cep"]').value = json.cep
+    q('input[name="address"]').value = json.logradouro
+    q('input[name="address"]').setAttribute('disabled', 'disabled')
+    q('input[name="neighborhood"]').value = json.bairro
+    q('input[name="neighborhood"]').setAttribute('disabled', 'disabled')
+    q('input[name="city"]').value = json.localidade
+    q('input[name="city"]').setAttribute('disabled', 'disabled')
+    q('input[name="state"]').value = json.uf
+    q('input[name="state"]').setAttribute('disabled', 'disabled')
 }
 
 q('input[name="cep"]').addEventListener('focusout', getAddress)
